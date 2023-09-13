d88bf3e53f464974e483701decc7a6dd2d7892c2
NuclleaR <cristobalruiz1994@gmail.com>
2023-09-13 20:12:04 -0300

    diseño de interfaz de formulario

---
+++ app/src/main/java/cristobal/ruiz/formulario_android/GetActivity.kt
@@ -0,0 +1,11 @@
+package cristobal.ruiz.formulario_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get)
+    }
+}
