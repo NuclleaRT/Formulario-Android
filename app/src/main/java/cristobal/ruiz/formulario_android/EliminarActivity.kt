32e597ee04883a9e278beb71f5d9c765474c99c9
NuclleaRT <cristobalruiz1994@gmail.com>
2023-10-03 21:03:36 -0300

    añadido GET con listWiew, comenzando a dearrollar Eliminar

---
+++ app/src/main/java/cristobal/ruiz/formulario_android/EliminarActivity.kt
@@ -0,0 +1,13 @@
+package cristobal.ruiz.formulario_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EliminarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eliminar)
+    }
+
+
+}
--- app/src/main/java/cristobal/ruiz/formulario_android/VerDatosActivity.kt
+++ app/src/main/java/cristobal/ruiz/formulario_android/VerDatosActivity.kt
@@ -1,143 +1,86 @@
 package cristobal.ruiz.formulario_android
 
-import android.annotation.SuppressLint
-import android.app.AlertDialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-
 
 class VerDatosActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: UsuarioAdapter
+    private lateinit var listViewUsuarios: ListView
+    private lateinit var adapter: ArrayAdapter<String> // Cambiar el tipo según tus necesidades
+    private lateinit var db: SQLite
+    private lateinit var usuarios: MutableList<String> // Cambiar el tipo según tus necesidades
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ver_datos)
 
-        recyclerView = findViewById(R.id.recyclerViewUsuarios)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        // Inicializar la base de datos SQLite utilizando tu clase SQLiteOpenHelper
+        db = SQLite(this, "RegistrarUsuario", null, 1)
 
-        // Obtener la lista de usuarios de la base de datos
-        val usuarios = obtenerTodosLosUsuarios()
+        // 1. Obtener una referencia al ListView desde el diseño XML
+        listViewUsuarios = findViewById(R.id.listViewUsuarios)
 
-        // Configurar el adaptador con la lista de usuarios
-        adapter = UsuarioAdapter(usuarios)
-        recyclerView.adapter = adapter
+        // 2. Obtener los datos de los usuarios de la base de datos
+        usuarios = obtenerTodosLosUsuarios()
+
+        // 3. Configurar un adaptador para el ListView
+        adapter = ArrayAdapter(this, R.layout.item_usuario, usuarios)
+
+        // 4. Asignar el adaptador al ListView
+        listViewUsuarios.adapter = adapter
+
+        // 5. Configurar OnClickListener para los botones "Eliminar"
+        configurarOnClickEliminar()
     }
 
-    // Función para obtener todos los usuarios de la base de datos
-    private fun obtenerTodosLosUsuarios(): List<Usuario> {
-        val usuarios = mutableListOf<Usuario>()
-        val con = SQLite(this, "RegistroUsuario", null, 1)
-        val baseDatos = con.readableDatabase
+    private fun obtenerTodosLosUsuarios(): MutableList<String> {
+        val usuarios = mutableListOf<String>()
 
-        val cursor = baseDatos.rawQuery("SELECT * FROM Usuario", null)
-        if (cursor.moveToFirst()) {
-            do {
-                val nombre = cursor.getString(cursor.getColumnIndex("USU_NOMBRE"))
-                val apellido = cursor.getString(cursor.getColumnIndex("USU_APELLIDO"))
-                val rut = cursor.getString(cursor.getColumnIndex("USU_RUT"))
-                val edad = cursor.getString(cursor.getColumnIndex("USU_EDAD"))
-                val telefono = cursor.getString(cursor.getColumnIndex("USU_TELEFONO"))
-                val fechaNacimiento =
-                    cursor.getString(cursor.getColumnIndex("USU_FECHANACIMIENTO"))
-                val tieneHijos = cursor.getString(cursor.getColumnIndex("USU_HIJOS"))
+        // Consultar todos los registros de la tabla "Usuario"
+        val query = "SELECT * FROM Usuario"
+        val cursor = db.readableDatabase.rawQuery(query, null)
 
-                val usuario =
-                    Usuario(nombre, apellido, edad, rut, telefono, fechaNacimiento, tieneHijos)
-                usuarios.add(usuario)
-            } while (cursor.moveToNext())
+        while (cursor.moveToNext()) {
+            val rut = cursor.getString(cursor.getColumnIndex("USU_RUT"))
+            val nombre = cursor.getString(cursor.getColumnIndex("USU_NOMBRE"))
+            val apellido = cursor.getString(cursor.getColumnIndex("USU_APELLIDO"))
+            val edad = cursor.getInt(cursor.getColumnIndex("USU_EDAD"))
+            val telefono = cursor.getString(cursor.getColumnIndex("USU_TELEFONO"))
+            val fechaNacimiento = cursor.getString(cursor.getColumnIndex("USU_FECHANACIMIENTO"))
+            val tieneHijos = cursor.getString(cursor.getColumnIndex("USU_HIJOS"))
+
+            // Formatea la información de los usuarios como desees
+            val usuarioInfo = "Nombre: $nombre $apellido\nRUT: $rut\nEdad: $edad\nTeléfono: $telefono\nFecha de Nacimiento: $fechaNacimiento\nTiene Hijos: $tieneHijos"
+            usuarios.add(usuarioInfo)
         }
 
         cursor.close()
-        baseDatos.close()
-
         return usuarios
     }
 
-    // Función para mostrar el diálogo de confirmación
-    fun mostrarDialogoConfirmacion(rut: String) {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Confirmación")
-            .setMessage("¿Deseas eliminar este usuario?")
-            .setPositiveButton("Aceptar") { _, _ ->
-                eliminarUsuario(rut)
+    private fun configurarOnClickEliminar() {
+        for (i in 0 until usuarios.size) {
+            val btnEliminar = listViewUsuarios.getChildAt(i)?.findViewById<Button>(R.id.btnEliminarUsuario)
+            btnEliminar?.setOnClickListener {
+                // Aquí puedes obtener el usuario asociado al botón haciendo referencia a la posición "i"
+                val usuario = usuarios[i]
+                eliminarUsuario(usuario)
+                // Actualiza la lista después de eliminar el usuario
+                usuarios.removeAt(i)
+                adapter.notifyDataSetChanged()
             }
-            .setNegativeButton("Cancelar") { dialog, _ ->
-                dialog.dismiss()
-            }
-            .create()
-            .show()
-    }
-
-    // Función para eliminar al usuario en la base de datos SQLite
-    private fun eliminarUsuario(rut: String) {
-        val con = SQLite(this, "RegistroUsuario", null, 1)
-        val baseDatos = con.writableDatabase
-
-        val cant = baseDatos.delete("Usuario", "USU_RUT=?", arrayOf(rut))
-        baseDatos.close()
-
-        if (cant > 0) {
-            Toast.makeText(this, "Usuario Eliminado Correctamente", Toast.LENGTH_LONG).show()
-            // Actualizar el RecyclerView después de eliminar el usuario
-            adapter.notifyDataSetChanged()
-        } else {
-            Toast.makeText(this, "Usuario no fue encontrado", Toast.LENGTH_LONG).show()
         }
     }
 
-    // Adaptador para el RecyclerView
-    inner class UsuarioAdapter(private val usuarios: List<Usuario>) :
-        RecyclerView.Adapter<UsuarioAdapter.UsuarioViewHolder>() {
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsuarioViewHolder {
-            val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_usuario, parent, false)
-            return UsuarioViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: UsuarioViewHolder, position: Int) {
-            val usuario = usuarios[position]
-
-            holder.nombreTextView.text = "Nombre: ${usuario.nombre}"
-            holder.apellidoTextView.text = "Apellido: ${usuario.apellido}"
-            holder.rutTextView.text = "Rut: ${usuario.rut}"
-            holder.edadTextView.text = "Edad: ${usuario.edad}"
-            holder.telefonoTextView.text = "Telefono: ${usuario.telefono}"
-            holder.fechaNacimientoTextView.text =
-                "Fecha de Nacimiento: ${usuario.fechaNacimiento}"
-            holder.hijosTextView.text = "¿Tiene hijos?: ${usuario.tieneHijosTexto}"
-
-            holder.btnEliminar.setOnClickListener {
-                mostrarDialogoConfirmacion(usuario.rut)
-            }
-        }
-
-        override fun getItemCount(): Int {
-            return usuarios.size
-        }
-
-        inner class UsuarioViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            val nombreTextView: TextView = itemView.findViewById(R.id.textViewNombre)
-            val apellidoTextView: TextView = itemView.findViewById(R.id.textViewApellido)
-            val rutTextView: TextView = itemView.findViewById((R.id.textViewRut))
-            val edadTextView: TextView = itemView.findViewById(R.id.textViewEdad)
-            val telefonoTextView: TextView = itemView.findViewById(R.id.textViewTelefono)
-            val fechaNacimientoTextView: TextView =
-                itemView.findViewById(R.id.textViewFechaNacimiento)
-            val hijosTextView: TextView = itemView.findViewById(R.id.textViewTieneHijos)
-            val btnEliminar: Button = itemView.findViewById(R.id.btnEliminarUsuario)
-        }
+    private fun eliminarUsuario(usuario: String) {
+        // Realiza la eliminación del usuario en la base de datos utilizando su ID o rut
+        val db = db.writableDatabase
+        val whereClause = "USU_RUT = ?"
+        val whereArgs = arrayOf(rut) // Asegúrate de que tengas un campo "rut" en la clase Usuario
+        db.delete("Usuario", whereClause, whereArgs)
+        db.close()
     }
 }
